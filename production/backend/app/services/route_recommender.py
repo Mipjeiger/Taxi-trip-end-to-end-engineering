b6617025c07b009c5578a902e8c8e147a067485e
Mipjeiger <dianhinhin123@gmail.com>
2026-05-02 10:52:13 +0800

    update repo streak

--- production/backend/app/services/route_recommender.py
+++ production/backend/app/services/route_recommender.py
@@ -20,5 +20,6 @@ class RouteRecommender:
             df = self.data
             # logic to find best route based on dataframe source by historical column and user preferences
             return {
-                "route": 
+                "route": "Mock Route",
+                "estimated_time": "30 mins",
             }
